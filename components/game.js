4a58f041141d4f700f2d06da36468b71985cf98f
Geoff Kimball <geoff@geoffkimball.com>
2017-06-23 15:20:30 -0700

    Move all game logic out of the <Game /> component and into its own separate class

--- components/game.js
+++ components/game.js
@@ -1,81 +1,26 @@
 import React, { Component } from 'react';
 import DocumentEvents from 'react-document-events';
-import rot from 'rot-js';
-import { autorun } from 'mobx';
-import WorldStore from '../stores/world';
+import Game from '../util/game';
 
-export default class Game extends Component {
-  world = new WorldStore()
+export default class GameViewer extends Component {
+  state = {
+    game: null
+  }
 
   componentDidMount() {
-    this.setupDisplay();
-    this.setupWorld();
-    this.tick();
-  }
+    const game = new Game();
+    this.container.appendChild(game.getCanvas());
+    game.start();
 
-  setupDisplay() {
-    this.display = new rot.Display({
-      width: 48,
-      height: 27,
-      fontSize: 16,
-      bg: '#333',
-      fg: '#fff',
-    });
-
-    const canvas = this.display.getContainer();
-    canvas.classList.add('game-container');
-
-    this.container.appendChild(canvas);
-  }
-
-  setupWorld() {
-    this.world.addEntity('player', 5, 5);
-    this.world.addEntity('gremlin', 10, 10);
-  }
-
-  handleKey = (e) => {
-    e.preventDefault();
-
-    switch (e.key) {
-      case 'ArrowUp':
-        this.world.player.moveUp();
-        this.tick();
-        break;
-      case 'ArrowDown':
-        this.world.player.moveDown();
-        this.tick();
-        break;
-      case 'ArrowLeft':
-        this.world.player.moveLeft();
-        this.tick();
-        break;
-      case 'ArrowRight':
-        this.world.player.moveRight();
-        this.tick();
-        break;
-      default:
-    }
-  }
-
-  tick() {
-    // Run entity AI
-    this.world.entities.forEach(entity => entity.living && entity.tick());
-
-    // Draw map
-    this.world.map.create((x, y, wall) => {
-      this.display.draw(x, y, wall ? '#' : '.');
-    });
-
-    // Draw entities
-    this.world.entities.map(({ x, y, character }) => {
-      this.display.draw(x, y, character);
-    });
+    this.setState({ game });
   }
 
   render() {
+    const { game } = this.state;
+
     return (
       <div className="container" ref={e => { this.container = e; }}>
-        <DocumentEvents onKeyDown={this.handleKey} />
+        {game && <DocumentEvents onKeyDown={game.handleKey} />}
         <style jsx>{`
           .container {
             width: 100%;
---
+++ util/game.js
@@ -0,0 +1,72 @@
+import rot from 'rot-js';
+import WorldStore from '../stores/world';
+import { autorun } from 'mobx';
+
+export default class Game {
+  constructor() {
+    this.display = new rot.Display({
+      width: 48,
+      height: 27,
+      fontSize: 16,
+      bg: '#333',
+      fg: '#fff',
+    });
+    this.world = new WorldStore();
+    this.setup();
+  }
+
+  // @TODO: Replace with something more declarative
+  setup() {
+    this.world.addEntity('player', 5, 5);
+    this.world.addEntity('gremlin', 10, 10);
+  }
+
+  getCanvas() {
+    const canvas = this.display.getContainer();
+    canvas.classList.add('game-container');
+    return canvas;
+  }
+
+  handleKey = (e) => {
+    e.preventDefault();
+
+    switch (e.key) {
+      case 'ArrowUp':
+        this.world.player.moveUp();
+        this.tick();
+        break;
+      case 'ArrowDown':
+        this.world.player.moveDown();
+        this.tick();
+        break;
+      case 'ArrowLeft':
+        this.world.player.moveLeft();
+        this.tick();
+        break;
+      case 'ArrowRight':
+        this.world.player.moveRight();
+        this.tick();
+        break;
+      default:
+    }
+  }
+
+  start() {
+    this.tick();
+  }
+
+  tick() {
+    // Run entity AI
+    this.world.entities.forEach(entity => entity.living && entity.tick());
+
+    // Draw map
+    this.world.map.create((x, y, wall) => {
+      this.display.draw(x, y, wall ? '#' : '.');
+    });
+
+    // Draw entities
+    this.world.entities.map(({ x, y, character }) => {
+      this.display.draw(x, y, character);
+    });
+  }
+}
