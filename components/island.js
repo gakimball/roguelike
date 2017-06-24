0f544149fc880e06d2a100a92da68092f28a6e62
Geoff Kimball <geoff@geoffkimball.com>
2017-06-23 20:09:36 -0700

    Move island generation logic into a separate file

--- components/island.js
+++ components/island.js
@@ -1,25 +1,12 @@
 import React, { Component } from 'react';
 import rot from 'rot-js';
-import scaleValue from 'scale-value';
-
-const scale = scaleValue(-1, 1, 0, 1);
-
-// Width and height of world
-const worldSize = 100;
-
-// Density of noise sampled for map
-// Higher numbers create simpler terrain
-const noiseDensity = 23;
-
-// Intensity of radial gradient that overlaps noise
-// Lower numbers prevent the islands from touching the edge of the map
-const radialIntensity = 65;
+import { createIsland, tiles, presets } from '../util/create-island';
 
 export default class Island extends Component {
   componentDidMount() {
     this.display = new rot.Display({
-      width: worldSize,
-      height: worldSize,
+      width: presets.default.worldSize,
+      height: presets.default.worldSize,
       fontSize: 8,
       forceSquareRatio: true
     });
@@ -28,34 +15,11 @@ export default class Island extends Component {
   }
 
   generate = () => {
-    const noise = new rot.Noise.Simplex();
-    const center = worldSize / 2;
+    const map = createIsland(presets.default);
 
-    for (let x = 0; x < worldSize; x++) {
-      for (let y = 0; y < worldSize; y++) {
-        const distanceX = (center - x) ** 2;
-        const distanceY = (center - y) ** 2;
-        const distanceToCenter = Math.sqrt(distanceX + distanceY) / radialIntensity;
-        const val = scale(noise.get(x / noiseDensity, y / noiseDensity)) - distanceToCenter;
-        let color;
-
-        if (val < 0.05) {
-          color = 'rgb(84, 185, 235)'; // Water
-        } else if (val < 0.175) {
-          color = 'rgb(212, 186, 95)'; // Beach
-        } else if (val < 0.5) {
-          color = 'rgb(93, 194, 100)'; // Low grass
-        } else if (val < 0.7) {
-          color = 'rgb(80, 170, 86)'; // High grass
-        } else if (val < 0.8) {
-          color = 'rgb(139, 122, 79)'; // Low mountain
-        } else if (val < 0.95) {
-          color = 'rgb(110, 96, 63)'; // High mountain
-        } else {
-          color = 'rgb(168, 38, 38)'; // Volcano
-        }
-
-        this.display.draw(x, y, '', '#fff', color);
+    for (let y = 0; y < presets.default.worldSize; y++) {
+      for (let x = 0; x < presets.default.worldSize; x++) {
+        this.display.draw(x, y, '', '#fff', map[y][x]);
       }
     }
   }
---
+++ util/create-island.js
@@ -0,0 +1,68 @@
+import rot from 'rot-js';
+import scaleValue from 'scale-value';
+
+const scale = scaleValue(-1, 1, 0, 1);
+
+const tiles = {
+  water: 'rgb(84, 185, 235)',
+  beach: 'rgb(212, 186, 95)',
+  lowGrass: 'rgb(93, 194, 100)',
+  highGrass: 'rgb(80, 170, 86)',
+  lowMountain: 'rgb(139, 122, 79)',
+  highMountain: 'rgb(110, 96, 63)',
+  volcano: 'rgb(168, 38, 38)',
+};
+
+const presets = {
+  default: {
+    // Width and height of world
+    worldSize: 100,
+    // Density of noise sampled for map
+    // Higher numbers create simpler terrain
+    noiseDensity: 23,
+    // Intensity of radial gradient that overlaps noise
+    // Lower numbers prevent the islands from touching the edge of the map
+    radialIntensity: 65,
+  },
+};
+
+export { tiles, presets };
+
+export function createIsland(preset = presets.default) {
+  const { worldSize, noiseDensity, radialIntensity } = preset;
+  const noise = new rot.Noise.Simplex();
+  const center = worldSize / 2;
+  const map = [];
+
+  for (let y = 0; y < worldSize; y++) {
+    map[y] = [];
+
+    for (let x = 0; x < worldSize; x++) {
+      const distanceX = (center - x) ** 2;
+      const distanceY = (center - y) ** 2;
+      const distanceToCenter = Math.sqrt(distanceX + distanceY) / radialIntensity;
+      const val = scale(noise.get(x / noiseDensity, y / noiseDensity)) - distanceToCenter;
+      let tile;
+
+      if (val < 0.05) {
+        tile = tiles.water;
+      } else if (val < 0.175) {
+        tile = tiles.beach;
+      } else if (val < 0.5) {
+        tile = tiles.lowGrass;
+      } else if (val < 0.7) {
+        tile = tiles.highGrass;
+      } else if (val < 0.8) {
+        tile = tiles.lowMountain;
+      } else if (val < 0.95) {
+        tile = tiles.highMountain;
+      } else {
+        tile = tiles.volcano;
+      }
+
+      map[y][x] = tile;
+    }
+  }
+
+  return map;
+};
